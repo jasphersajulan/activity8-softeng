e4dac2d38807115829a5eeb242a5586cb00309cf
Jaspher Sajulan <youremail@example.com>
2025-05-07 23:49:16 +0800

    Initial commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Welcome to the DevOps App!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0,0.0.0', port=5000)
